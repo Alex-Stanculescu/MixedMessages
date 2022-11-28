5c91862340acbdde495b5f0dee3d6a0ba92ea780
Alex <alx.stanculescu@gmail.com>
2022-11-28 13:17:58 +0100

    fixed the randon number function + polished code

--- index.js
+++ index.js
@@ -2,10 +2,9 @@ let info1 = ["Capricorn", "Aquarius", "Pisces", "Aries", "Taurus", "Gemini", "Ca
 let info2 = ["smart", "able", "absent-minded", "agile", "amazing", "amusing", "apathetic", "artificial", "attentive", "awful", "adventurous", "agreeable", "analytical"];
 let info3 = ["Confucius", "Aristotle", "Plato", "Socrates", "David Hume", "Rene Descartes", "Wilhelm Hegel", "Immanuel Kant", "Friedrich Nietzsche", "Epicurus", "Laozi", "Voltaire", "Karl Marx", "Jean-Jacques Rousseau"];
 
-let randomNumber = (Array) => Math.floor(Math.random() * Array.length)
-let randomInfo1 = info1[randomNumber(info1)];
-let randomInfo2 = info2[randomNumber(info2)];
-let randomInfo3 = info3[randomNumber(info3)];
+const randomNumber = (array) => {return array[Math.floor(Math.random() * array.length)]}
+let randomInfo1 = randomNumber(info1);
+let randomInfo2 = randomNumber(info2);
+let randomInfo3 = randomNumber(info3);
 
-let displayMessage = `${randomInfo3} says that if you're a ${randomInfo1}, then you're probably ${randomInfo2}`
-
+let displayMessage = `Today's piece of advice: ${randomInfo3} says that if you're a ${randomInfo1}, then you're probably ${randomInfo2}`;
